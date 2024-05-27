7fbf1ec74184ae70dca9561d17fecddb47c64f73
Nik Anand <mntechsurvey@gmail.com>
2024-05-27 00:07:46 -0400

    fixed api

--- index.js
+++ index.js
@@ -4,23 +4,28 @@ const fetch = require("node-fetch");
 
 const cors = require("cors");
 app.use(cors({
-        origin: 'https://remoteaccess--startling-kelpie-5c183f.netlify.app/'
+        origin: '*'
 }));
 
 app.get("/player/:name/seasonStats", async (req, res) => {
         const requestOptions = {
             method: 'GET',
+                headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `8164e6c8-9e68-4c31-929c-1872397034da`  // Replace with your actual API key
+                }
         };
         const headers = {'Content-Type':'application/json',
                 'Access-Control-Allow-Origin':'*',
                 'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'}
         //TODO: limit player info result to 1 (loop through data removing based an anything null)
-        let playerInfo = await fetch("https://balldontlie.io/api/v1/players?search="+req.params.name, requestOptions);
+        let playerInfo = await fetch("https://api.balldontlie.io/v1/players?search="+req.params.name, requestOptions);
         let playerInfoJson = await playerInfo.json();
+        console.log(playerInfoJson)
         try {
                 let playerID = playerInfoJson.data[0].id;
 
-                const playerSeasonStatsReq = await fetch("https://balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=" + playerID, requestOptions);
+                const playerSeasonStatsReq = await fetch("https://api.balldontlie.io/v1/season_averages?season=2023&player_ids[]=" + playerID, requestOptions);
                 const playerSeasonStats = await playerSeasonStatsReq.json();
                 //console.log(JSON.stringify(playerSeasonStats).substring(0,JSON.stringify(playerSeasonStats).indexOf('}')+1))
                 //console.log('\n\n\n')
